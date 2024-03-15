f015a3310eaef1699ccded3a718de63305f9731e
kyf <kyf@kyfs-MacBook-Air.local>
2024-03-16 03:19:42 +0800

    1.1finish

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.name}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Content = (props) => {
+  return (
+    <p>{props.name} {props.num}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.num}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content name={part1} num={exercises1} />
+      <Content name={part2} num={exercises2} />
+      <Content name={part3} num={exercises3} />
+      <Total num = {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
