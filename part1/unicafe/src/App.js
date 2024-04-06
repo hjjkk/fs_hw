50ea1103e6bee3de4111a2029f28978c374f6742
hjjkk <kongyf1999@163.com>
2024-04-06 22:25:02 +0800

    1.8 finish

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,14 +6,32 @@ const Button = (props) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total
+  if (total === 0) {
+    return (<p>No feedback given</p>)
+  }
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   return (
-
     <div>
       <h1>give feedback</h1>
       <div>
@@ -24,9 +42,10 @@ const App = () => {
 
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+
     </div>
   )
 }
